2c51e7145f29e1194c5d1e3727d6b5124658d1fb
Sean Gowen <sean.r.gowen@gmail.com>
2016-02-22 21:54:31 -0500

    test from desktop

---
+++ app/src/main/java/com/rustandtuna/gameprototype/PlayerPiece.java
@@ -0,0 +1,57 @@
+package com.rustandtuna.gameprototype;
+
+/**
+ * Created by Sean on 2/8/2016.
+ */
+public class PlayerPiece {
+    private String type;
+    private int HP;
+    private int coord;
+    int maxHp = 20;
+    int p1p2;
+
+
+//  Only accepts "ranged","close", or "mounted"
+    public PlayerPiece(int p1p2, String type) {
+        this.p1p2 = p1p2;
+
+        if (type == "ranged"|| type =="close" || type=="mounted"){
+            this.type = type;
+        }
+
+        this.HP = maxHp;
+        //this.coord = coord;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+//        this.type = type;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public void setHP(int HP) {
+//        this.HP = HP;
+    }
+
+    public int getCoord() {
+        return coord;
+    }
+
+    public void setCoord(int coord) {
+        this.coord = coord;
+    }
+
+    public int getP1p2() {
+        return p1p2;
+    }
+
+    public void inflictDamage(int damage){
+        HP = HP-damage;
+    }
+}
